2ecede2d90f4edfc7d2b5f97ba7a0c88aa44aec4
Alice52 <zzhang_xz@163.com>
2019-12-23 22:46:25 +0800

    feat: add spring boot integration web mvc

--- java/javaee/spring-boot/integration/src/main/java/com/augmentum/springboot/integration/configuration/WebMvcConfiguration.java
+++ java/javaee/spring-boot/integration/src/main/java/com/augmentum/springboot/integration/configuration/WebMvcConfiguration.java
@@ -1,7 +1,6 @@
 package com.augmentum.springboot.integration.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,17 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @create 2019-12-22 20:02
  * @function config mvc resource handler
  */
-@EnableWebMvc
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-    registry
-        .addResourceHandler("swagger-ui.html")
-        .addResourceLocations("classpath:/META-INF/resources/");
-    registry
-        .addResourceHandler("/webjars/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
   }
 }
