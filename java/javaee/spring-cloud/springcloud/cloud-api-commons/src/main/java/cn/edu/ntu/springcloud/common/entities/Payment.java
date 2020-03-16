c00d931aa24fa53e6137e1c89983605461e98c96
Alice52 <zzhang_xz@163.com>
2020-03-17 00:12:21 +0800

    feat: add spring cloud sample about eureka

---
+++ java/javaee/spring-cloud/springcloud/cloud-api-commons/src/main/java/cn/edu/ntu/springcloud/common/entities/Payment.java
@@ -0,0 +1,19 @@
+package cn.edu.ntu.springcloud.common.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author zack <br>
+ * @create 2020-03-10 00:25 <br>
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+  private Long id;
+  private String serial;
+}
---
+++ java/javaee/spring-cloud/springcloud/cloud-provider-payment8001/src/main/java/cn/edu/ntu/springcloud/payment/service/PaymentService.java
@@ -0,0 +1,26 @@
+package cn.edu.ntu.springcloud.payment.service;
+
+import cn.edu.ntu.springcloud.common.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author zack <br/>
+ * @create 2020-03-10 00:49 <br/>
+ */
+public interface PaymentService {
+    /**
+     * Create payment record. <br>
+     *
+     * @param payment entity
+     * @return id
+     */
+    int create(Payment payment);
+
+    /**
+     * Get Payment info by id. <br>
+     *
+     * @param id pk
+     * @return Payment by id
+     */
+    Payment getPaymentById(@Param("id") Long id);
+}
