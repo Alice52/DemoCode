af4e28ab3e1e914474d7b6deabc30bc12d8cfe31
Alice52 <zzhang_xz@163.com>
2021-01-04 21:07:41 +0800

    boot/common: add new common api about api response and swagger 3.0

---
+++ java/javaee/spring-boot/integration/boot-cache/src/main/java/boot/cache/CacheApplication.java
@@ -0,0 +1,16 @@
+package boot.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zack <br>
+ * @create 2021-01-04 20:59 <br>
+ * @project springboot <br>
+ */
+@SpringBootApplication
+public class CacheApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CacheApplication.class, args);
+  }
+}
