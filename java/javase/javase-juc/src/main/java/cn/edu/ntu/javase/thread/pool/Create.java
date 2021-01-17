bc768f5c9def60744bba2d77f60d21d72707660c
Alice52 <zzhang_xz@163.com>
2021-01-17 18:59:58 +0800

    java/se: add thread note about completable-future sample

---
+++ java/javase/javase-juc/src/main/java/cn/edu/ntu/javase/thread/pool/Create.java
@@ -0,0 +1,9 @@
+package cn.edu.ntu.javase.thread.pool;
+
+/**
+ * @see cn.edu.ntu.javase.thread.create.PoolCreate
+ * @author zack <br>
+ * @create 2021-01-17<br>
+ * @project javase <br>
+ */
+public class Create {}
