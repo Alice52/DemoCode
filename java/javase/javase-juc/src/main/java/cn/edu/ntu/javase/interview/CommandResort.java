9783f7957e6863772294b58567d5e6a6f9d09e50
Alice52 <zzhang_xz@163.com>
2021-01-18 23:23:22 +0800

    java/se: add volatile explain sample

---
+++ java/javase/javase-juc/src/main/java/cn/edu/ntu/javase/interview/CommandResort.java
@@ -0,0 +1,35 @@
+package cn.edu.ntu.javase.interview;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zack <br>
+ * @create 2021-01-18<br>
+ * @project javase <br>
+ */
+@Slf4j
+public class CommandResort {
+  int a = 0;
+  boolean flag = false;
+
+  /**
+   * 这里可能存在指令重排, 则在超多线程都在执行 m0, m2 时会出问题, 导致结果不唯一
+   *
+   * <pre>
+   *      1. flag 在第一句: 就会导致线程执行 m2 时, 且在 a=1 执行之前执行, a=5
+   *      2. flag 在第二句: 就会导致线程执行 m2 时,  a=6
+   *      3. solution: 在 a 和 flag 之前都加上 volatile 指定涉及到这两个的都不循序指令重排, 则 a=6
+   * </pre>
+   */
+  public void m0() {
+    a = 1;
+    flag = true;
+  }
+
+  public void m2() {
+    if (flag) {
+      a = a + 5;
+      log.info("a: {}", a);
+    }
+  }
+}
---
+++ java/javase/javase-juc/src/main/java/cn/edu/ntu/javase/interview/VolatileVisible.java
@@ -0,0 +1,54 @@
+package cn.edu.ntu.javase.interview;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zack <br>
+ * @create 2021-01-18<br>
+ * @project javase <br>
+ */
+@Slf4j
+public class VolatileVisible {
+
+  public static void main(String[] args) {
+    // 1. 资源类
+    VolatileData data = new VolatileData();
+
+    // 2. 第一个线程: 3秒之后修改为 60
+    new Thread(
+            () -> {
+              log.info("thread: {} come in", Thread.currentThread().getName());
+
+              try {
+                TimeUnit.SECONDS.sleep(3);
+              } catch (InterruptedException e) {
+              }
+              log.info(
+                  "thread: {} update number value to: {}",
+                  Thread.currentThread().getName(),
+                  data.number);
+
+              data.addTo60();
+            },
+            "AAA")
+        .start();
+
+    /**
+     * volatile 的可见性
+     *
+     * <pre>
+     *     1. number 前没有 volatile 关键字: 彼此线程不可见
+     *          - 会一直 block: main 线程在最初的时候加载到自己的工作空间的是 0, AAA 线程修改对 main 线程是不可见的[即使AAA把数据写回了主内存]
+     *     2. number 前有 volatile 关键字: 彼此线程可见[主内存实现]
+     *          - 则不会 block 在这里, main 可以拿到修改后的值
+     * </pre>
+     */
+    while (data.number == 0) {
+      // 只要不等于 0 则向下执行
+    }
+
+    log.info("thread: {} get number value is {}", Thread.currentThread().getName(), data.number);
+  }
+}
