3bb373c4477858dbdae7804f55ba3f8faa67fd96
Alice52 <zzhang_xz@163.com>
2021-03-02 00:27:56 +0800

    javase/juc: add lock-support smaple

---
+++ java/javase/javase-juc/src/main/java/cn/edu/ntu/javase/interview/lock/LockCount.java
@@ -0,0 +1,46 @@
+package cn.edu.ntu.javase.interview.lock;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 注意这里只会出现等待, 不是死锁
+ *
+ * @author zack <br>
+ * @create 2021-03-01 16:02 <br>
+ * @project javase <br>
+ */
+@Slf4j
+public class LockCount {
+
+  public static void main(String[] args) {
+    ReentrantLock lock = new ReentrantLock();
+
+    new Thread(
+            () -> {
+              lock.lock();
+              lock.lock();
+              try {
+                log.info("execute ...");
+              } finally {
+                lock.unlock();
+                lock.unlock();
+              }
+            },
+            "AA")
+        .start();
+
+    new Thread(
+            () -> {
+              lock.lock();
+              try {
+                log.info("execute ...");
+              } finally {
+                lock.unlock();
+              }
+            },
+            "B")
+        .start();
+  }
+}
