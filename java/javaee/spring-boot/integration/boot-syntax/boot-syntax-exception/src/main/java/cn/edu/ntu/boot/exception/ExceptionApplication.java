5fcc898781ff3e61f24b92bb3ff6c777d69de5e8
zack.zhang <zackzhang@augmentum.com.cn>
2021-04-26 18:33:29 +0800

    java/ee: add schedle job in spring native

--- java/javaee/spring-boot/integration/boot-syntax/boot-syntax-exception/src/main/java/cn/edu/ntu/boot/exception/ExceptionApplication.java
+++ java/javaee/spring-boot/integration/boot-syntax/boot-syntax-exception/src/main/java/cn/edu/ntu/boot/exception/ExceptionApplication.java
@@ -1,13 +1,12 @@
 package cn.edu.ntu.boot.exception;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author zack <br>
  * @create 2020-04-27 20:22 <br>
  */
-//@SpringBootApplication
+// @SpringBootApplication
 public class ExceptionApplication {
 
   public static void main(String[] args) {
