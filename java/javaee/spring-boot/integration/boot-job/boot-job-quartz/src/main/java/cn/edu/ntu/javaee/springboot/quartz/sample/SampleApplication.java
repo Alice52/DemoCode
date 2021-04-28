03762099053ae61f308e110aa4bf1922c6bef359
Alice52 <zzhang_xz@163.com>
2021-04-29 00:08:01 +0800

    java/ee: add boot integration about quartz job

---
+++ java/javaee/spring-boot/integration/boot-job/boot-job-quartz/src/main/java/cn/edu/ntu/javaee/springboot/quartz/sample/SampleApplication.java
@@ -0,0 +1,17 @@
+package cn.edu.ntu.javaee.springboot.quartz.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zack <br>
+ * @create 2021-04-28<br>
+ * @project integration <br>
+ */
+@SpringBootApplication
+public class SampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SampleApplication.class, args);
+  }
+}
