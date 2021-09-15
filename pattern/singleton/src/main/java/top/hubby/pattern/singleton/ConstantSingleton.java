4f9133a3e154031be28b77fcc3e3f104a6f564c8
zack.zhang <zackzhang@augmentum.com.cn>
2021-09-15 16:52:42 +0800

    pattern: refine singleton samples

--- pattern/singleton/src/main/java/top/hubby/pattern/singleton/ConstantSingleton.java
+++ pattern/singleton/src/main/java/top/hubby/pattern/singleton/ConstantSingleton.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ConstantSingleton {
 
-    public static final ConstantSingleton instance = new ConstantSingleton();
+    public static /*final*/ ConstantSingleton instance = new ConstantSingleton();
 
     private ConstantSingleton() {}
 
