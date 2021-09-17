2cdab921e31a9a6b339026358039164baa83a36e
zack.zhang <zackzhang@augmentum.com.cn>
2021-09-17 16:17:36 +0800

    pattern: add pattern design principle sample

---
+++ pattern/common/src/main/java/top/hubby/principles/isp/A.java
@@ -0,0 +1,12 @@
+package top.hubby.principles.isp;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author asd <br/>
+ * @create 2021-09-17 3:57 PM <br/>
+ * @project pattern <br/>
+ */
+@Slf4j
+public class A {
+}
