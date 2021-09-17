658197d8250f21cb951ce362ef20c3566363495e
zack.zhang <zackzhang@augmentum.com.cn>
2021-09-17 16:17:36 +0800

    pattern: add pattern design principle sample

--- pattern/common/src/main/java/top/hubby/principles/di/before/Computer.java
+++
@@ -1,8 +0,0 @@
-package top.hubby.principles.di.before;
-
-/**
- * @author zack <br>
- * @create 2021-09-13<br>
- * @project pattern <br>
- */
-public class Computer {}
---
+++ pattern/common/src/main/java/top/hubby/principles/ocp/Macbook.java
@@ -0,0 +1,12 @@
+package top.hubby.principles.ocp;
+
+import lombok.extern.slf4j.Slf4j;
+import top.hubby.principles.ocp.Computer;
+
+/**
+ * @author asd <br>
+ * @create 2021-09-17 2:58 PM <br>
+ * @project pattern <br>
+ */
+@Slf4j
+public class Macbook implements Computer {}
