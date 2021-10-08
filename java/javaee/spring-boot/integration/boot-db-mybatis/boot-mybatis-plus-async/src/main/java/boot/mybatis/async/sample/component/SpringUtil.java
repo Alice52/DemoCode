0fdb5437da46c99d1679898e72137f0f278ac876
zack.zhang <zackzhang@augmentum.com.cn>
2021-10-08 13:57:23 +0800

    java/boot: refine spring boot async sample

---
+++ java/javaee/spring-boot/integration/boot-db-mybatis/boot-mybatis-plus-async/src/main/java/boot/mybatis/async/sample/component/SpringUtil.java
@@ -0,0 +1,13 @@
+package boot.mybatis.async.sample.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author asd <br>
+ * @create 2021-09-30 2:31 PM <br>
+ * @project boot-mybatis-rwa <br>
+ */
+@Slf4j
+@Component
+public class SpringUtil extends cn.hutool.extra.spring.SpringUtil {}
--- java/javaee/spring-boot/integration/boot-db-mybatis/boot-mybatis-plus-async/src/main/java/boot/mybatis/async/sample/config/SpringAsyncConfiguration.java
+++ java/javaee/spring-boot/integration/boot-db-mybatis/boot-mybatis-plus-async/src/main/java/boot/mybatis/async/sample/config/SpringAsyncConfiguration.java
@@ -2,15 +2,15 @@ package boot.mybatis.async.sample.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
-
-import java.lang.reflect.Method;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
  * 自定义子线程 UncaughtException 处理器, 默认就可以捕获.
  *
- * @see AsyncPoolConfig
  * @author zack <br>
  * @create 2021-05-31 17:05 <br>
  * @project boot-mybatis-plus-async <br>
@@ -19,8 +19,30 @@ import java.lang.reflect.Method;
 @Slf4j
 public class SpringAsyncConfiguration implements AsyncConfigurer {
 
+    private static final int MAX_POOL_SIZE;
+    private static final int CORE_POOL_SIZE;
+    private static final int QUEUE_SIZE;
+
+    static {
+        int processors = Runtime.getRuntime().availableProcessors();
+        MAX_POOL_SIZE = (int) (processors / (1 - 0.9));
+        CORE_POOL_SIZE = processors;
+        QUEUE_SIZE = MAX_POOL_SIZE * 3;
+    }
+
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return (Throwable throwable, Method method, Object... obj) -> log.error("", throwable);
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }
+
+    @Override
+    public AsyncTaskExecutor getAsyncExecutor() {
+        ThreadPoolTaskExecutor asyncTaskExecutor = new ThreadPoolTaskExecutor();
+        asyncTaskExecutor.setMaxPoolSize(MAX_POOL_SIZE);
+        asyncTaskExecutor.setCorePoolSize(CORE_POOL_SIZE);
+        asyncTaskExecutor.setQueueCapacity(QUEUE_SIZE);
+        asyncTaskExecutor.setThreadNamePrefix("async-pool-");
+        asyncTaskExecutor.initialize();
+        return asyncTaskExecutor;
     }
 }
