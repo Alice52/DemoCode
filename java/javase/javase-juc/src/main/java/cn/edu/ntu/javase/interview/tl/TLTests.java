39a462c80ce5112c10c0a3790b74688d967edc30
Alice52 <zzhang_xz@163.com>
2021-10-13 22:55:14 +0800

    java/se: juc threadlocal

---
+++ java/javase/javase-juc/src/main/java/cn/edu/ntu/javase/interview/tl/TLTests.java
@@ -0,0 +1,46 @@
+package cn.edu.ntu.javase.interview.tl;
+
+import java.util.concurrent.TimeUnit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author zack <br>
+ * @create 2021-10-13<br>
+ * @project javase <br>
+ */
+@Slf4j
+public class TLTests {
+
+    @Test
+    public void testTLShare() {
+        ThreadLocal<String> tl = new ThreadLocal<String>();
+
+        new Thread(
+                        () -> {
+                            try {
+                                TimeUnit.SECONDS.sleep(2);
+                                log.info("ThreadLocal content: {}", tl.get());
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        })
+                .start();
+
+        new Thread(
+                        () -> {
+                            try {
+                                TimeUnit.SECONDS.sleep(1);
+                                tl.set("zack");
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        })
+                .start();
+
+        while (Thread.activeCount() > 2) {
+            Thread.yield();
+        }
+    }
+}
