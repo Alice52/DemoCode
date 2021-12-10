b33b5ec07b123fc0b8bee75b48e93c6b9a095e35
zack.zhang <zackzhang@augmentum.com.cn>
2021-12-10 17:37:39 +0800

    java/se: add inherit and compose sample

---
+++ pattern/common/src/main/java/top/hubby/inherit/v1/Tweetable.java
@@ -0,0 +1,12 @@
+package top.hubby.inherit.v1;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author asd <br/>
+ * @create 2021-12-10 10:46 AM <br/>
+ * @project pattern <br/>
+ */
+public interface Tweetable {
+    void tweet();
+}
