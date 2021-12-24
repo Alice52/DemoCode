288a903ff08461a6843033c56660af3fa632122c
zack.zhang <zackzhang@augmentum.com.cn>
2021-12-24 18:10:09 +0800

    pattern: add sample pratice about uid generator

--- pattern/common/src/main/java/top/hubby/inherit/v3/Flyable.java
+++ pattern/common/src/main/java/top/hubby/inherit/v3/Flyable.java
@@ -1,13 +1,10 @@
 package top.hubby.inherit.v3;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
- * @author asd <br/>
- * @create 2021-12-10 10:51 AM <br/>
- * @project pattern <br/>
+ * @author asd <br>
+ * @create 2021-12-10 10:51 AM <br>
+ * @project pattern <br>
  */
-
 public interface Flyable {
     void fly();
 }
