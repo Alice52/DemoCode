6884602d5f71cf15e2d8a17d64eb8e9735e96ea9
zack.zhang <zackzhang@augmentum.com.cn>
2021-12-27 14:07:37 +0800

    pattern: add metrics refactor flow

--- pattern/common/src/main/java/top/hubby/practice/metrics/store/impl/RedisMetricsStorage.java
+++ pattern/common/src/main/java/top/hubby/practice/metrics/store/impl/RedisMetricsStorage.java
@@ -1,6 +1,11 @@
 package top.hubby.practice.metrics.store.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import top.hubby.practice.metrics.model.RequestInfo;
+import top.hubby.practice.metrics.store.MetricsStorage;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author asd <br>
@@ -8,4 +13,18 @@ import lombok.extern.slf4j.Slf4j;
  * @project pattern <br>
  */
 @Slf4j
-public class RedisMetricsStorage {}
+public class RedisMetricsStorage implements MetricsStorage {
+    @Override
+    public void saveRequestInfo(RequestInfo requestInfo) {}
+
+    @Override
+    public List getRequestInfos(String apiName, long startTimeInMillis, long endTimeInMillis) {
+        return null;
+    }
+
+    @Override
+    public Map<String, List<RequestInfo>> getRequestInfos(
+            long startTimeInMillis, long endTimeInMillis) {
+        return null;
+    }
+}
