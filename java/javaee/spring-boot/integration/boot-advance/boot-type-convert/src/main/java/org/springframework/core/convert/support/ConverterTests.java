bdf7d94fde8af81d813c70a013276d16e604ee01
zack.zhang <zackzhang@augmentum.com.cn>
2022-02-08 18:25:10 +0800

    java/ee: add boot type-converter note

--- java/javaee/spring-boot/integration/boot-advance/boot-type-convert/src/main/java/org/springframework/core/convert/support/ConverterTests.java
+++ java/javaee/spring-boot/integration/boot-advance/boot-type-convert/src/main/java/org/springframework/core/convert/support/ConverterTests.java
@@ -1,7 +1,10 @@
-package hubby.typeconvert.converter;
+package org.springframework.core.convert.support;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.springframework.core.convert.converter.Converter;
+
+import java.nio.charset.Charset;
 
 /**
  * @author asd <br>
@@ -10,6 +13,8 @@ import org.junit.Test;
  */
 @Slf4j
 public class ConverterTests {
+
+    /** 这里需要将包名改为和 StringToBooleanConverter 一样的 */
     @Test
     public void test() {
         /*
@@ -18,13 +23,12 @@ public class ConverterTests {
            - yes   ||  no
            - 1     ||  0
         */
-        // Converter<String, Boolean> converter = new StringToBooleanConverter();
+        Converter<String, Boolean> converter = new StringToBooleanConverter();
 
         /*
             - uTf-8
             - utF8
         */
-        // Converter<String, Charset> converter2 = new StringToCharsetConverter();
-
+        Converter<String, Charset> converter2 = new StringToCharsetConverter();
     }
 }
